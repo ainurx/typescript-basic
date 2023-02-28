23e99083c8e51bf3e921b545dc0ce12312cf6aee
ainurx <ainurrahmat1@gmail.com>
2023-03-01 01:34:09 +0800

    [master] Create var with types and interface

--- index.ts
+++ index.ts
@@ -1,3 +1,16 @@
 let firstName = 'ainur'
+let age: number
+age = 24
 
 console.log(`Halo ${firstName}`)
+
+// create interface to define key types for obj
+interface User{
+    id: number,
+    fullName: string
+}
+
+const newUser: User = {
+    id: 1,
+    fullName: 'Ainur Rahmat'
+} 
