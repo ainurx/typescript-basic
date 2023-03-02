65754c96952d4d195b7690dd2ba172fde5906d69
ainurx <ainurrahmat1@gmail.com>
2023-03-02 17:10:24 +0800

    [master] Obj  & blueprint

--- index.ts
+++ index.ts
@@ -14,3 +14,16 @@ const newUser: User = {
     id: 1,
     fullName: 'Ainur Rahmat'
 } 
+
+class UserAccount{
+    id: number
+    fullName: string
+    
+    constructor(id: number, fullName: string){
+        this.id = id
+        this.fullName = fullName
+    }
+}
+
+const user: User = new UserAccount(1, "Viramelia")
+console.log(user)
