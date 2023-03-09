6e8c9e06d3aae1a8b7187dbc6d130944853e3c6f
ainurx <ainurrahmat1@gmail.com>
2023-03-09 18:21:52 +0800

    [master] Adding methods

--- index.ts
+++ index.ts
@@ -25,14 +25,40 @@ class StudentAccount {
     entryYear: EntryYear
     department: Department
 
-    constructor( username: string, password: string, fullName: string, semester: Semester, address: string, phone: string, entryYear: EntryYear, department: Department ){
+    // constructor( username: string, password: string, fullName: string, semester: Semester, address: string, phone: string, entryYear: EntryYear, department: Department ){
+    //     this.username = username
+    //     this.password = password
+    //     this.fullName = fullName
+    //     this.semester = semester
+    //     this.address = address
+    //     this.phone = phone
+    //     this.entryYear = entryYear
+    //     this.department = department
+    // }
+
+    public constructor(username: string){
         this.username = username
-        this.password = password
-        this.fullName = fullName
-        this.semester = semester
-        this.address = address
-        this.phone = phone
-        this.entryYear = entryYear
-        this.department = department
+    }
+
+    // public addStudent(username: string, password: string): void{
+    //     this.username = username
+    //     this.password = password
+    // }
+
+    public getStudent(): {username: string, password: string}{
+        return {
+            username: this.username,
+            password: this.password
+        }
+    }
+
+    public getUsername(): string {
+        return this.username
     }
 }
+
+const newStudent = new StudentAccount("Ainur")
+// newStudent.addStudent("Ainur", "Viramelia")
+console.log(newStudent.getStudent())
+console.log(newStudent.getUsername())
+
